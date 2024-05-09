7fe1b653c3009d9481811aaf859d6813b0a3f3f4
Eric Rizzi <eric.rizzi@gmail.com>
2024-05-09 18:41:03 -0500

    Remove unused global variables

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,7 @@ var _____WB$wombat$assign$function_____ = function (name) { return (self._wb_wom
 if (!self.__WB_pmw) { self.__WB_pmw = function (obj) { this.__WB_source = obj; return this; } }
 {
     let window = _____WB$wombat$assign$function_____("window");
-    let self = _____WB$wombat$assign$function_____("self");
     let document = _____WB$wombat$assign$function_____("document");
-    let location = _____WB$wombat$assign$function_____("location");
-    let top = _____WB$wombat$assign$function_____("top");
-    let parent = _____WB$wombat$assign$function_____("parent");
-    let frames = _____WB$wombat$assign$function_____("frames");
-    let opener = _____WB$wombat$assign$function_____("opener");
 
     class Robozzle {
         urlCallback: any;
